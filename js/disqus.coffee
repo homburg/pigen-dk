e329dc3f9c4655d2ae9ee21f4e1a5e80bfa15b4b
Thomas B Homburg <thomas@homburg.dk>
2012-02-02 21:01:14 +0100

    Created disqus coffeescript class and fixed disqus functionality across dynamic refreshes

---
+++ js/disqus.coffee
@@ -0,0 +1,25 @@
+class Disqus
+	# required: replace example with your forum shortname
+	constructor: (@shortname)->
+		shortname = window.disqus_shortname = @shortname
+		@dsq = $("<script/>").attr
+			'type': 'text/javascript',
+			'async': 'async',
+			'src': "http://#{shortname}.disqus.com/embed.js"
+	
+	load: (url = null) ->
+		window.disqus_url = url
+		$('head').append(@dsq)
+
+	unload: ->
+		$(@dsq).remove()
+		$("#disqus_thread").empty()
+
+	reload: (url = none) ->
+		@unload()
+		@load(url)
+
+if !window?.d
+	window.d = new Disqus 'puopuo'
+	window.d.load()
+	console.log(window.d)
---
+++ js/disqus.js
@@ -0,0 +1,41 @@
+(function() {
+  var Disqus;
+
+  Disqus = (function() {
+
+    function Disqus(shortname) {
+      this.shortname = shortname;
+      shortname = window.disqus_shortname = this.shortname;
+      this.dsq = $("<script/>").attr({
+        'type': 'text/javascript',
+        'async': 'async',
+        'src': "http://" + shortname + ".disqus.com/embed.js"
+      });
+    }
+
+    Disqus.prototype.load = function(url) {
+      if (url == null) url = null;
+      window.disqus_url = url;
+      return $('head').append(this.dsq);
+    };
+
+    Disqus.prototype.unload = function() {
+      $(this.dsq).remove();
+      return $("#disqus_thread").empty();
+    };
+
+    Disqus.prototype.reload = function(url) {
+      if (url == null) url = none;
+      this.unload();
+      return this.load(url);
+    };
+
+    return Disqus;
+
+  })();
+
+  window.d = new Disqus('puopuo');
+
+  window.d.load();
+
+}).call(this);
