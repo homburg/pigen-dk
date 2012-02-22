deea0b26acd1538c08757bb2c1146a0eb0a9deb1
Thomas B Homburg <thomas@homburg.dk>
2012-02-22 21:34:14 +0100

    Fixed fb curl for development

---
+++ scripts/fb_yaml.php
@@ -0,0 +1,54 @@
+#!/usr/bin/env php
+<?php
+
+error_reporting(E_ALL);
+
+class FbCurler {
+	public static function main (array $argv) {
+
+		$callStr = "call: \"".implode($argv, ' ')."\"\n";
+		shell_exec('logger '.$callStr);
+		echo $callStr;
+
+		if (!isset($argv[1]))
+			return;
+
+		$outDir = 'panels/yaml_lock';
+		if (is_file($outDir)) {
+			echo "Illegal file: ".$outDir;
+			exit(1);
+		}
+
+		if (!is_dir($outDir)) {
+			mkdir($outDir);
+		}
+
+		$file = basename($argv[1]);
+		$count = 0;
+		$id = preg_replace('/\.yaml$/', '', $file, 1, $count);
+
+		if (1 != $count)
+		{
+			echo "Invalid input file: \"".$argv[1]."\"\n";
+			exit(1);
+		}
+
+		// I for one can't wait for array literals
+		if (gethostname() === "gravko")
+			$domains = array('test.pigen.dk');
+		else
+			$domains = array('pigen.dk', 'www.pigen.dk');
+		$out = "";
+		foreach ($domains as $domain) {
+			$url  = "http://${domain}/${id}";
+			$fbUrl = 'http://developers.facebook.com/tools/debug/og/object?format=json&q='.rawurlencode($url);
+			shell_exec("logger Getting: ${fbUrl}\n");
+			$out = file_get_contents($fbUrl);
+		}
+
+		file_put_contents($outDir.'/'.$file, $out);
+	}
+}
+
+FbCurler::main($argv);
+
