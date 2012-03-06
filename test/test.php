bc40e255e5cd5261e9653d309b22dccfe5dc8f16
Thomas Bondo Homburg <thomas@homburg.dk>
2012-03-06 20:34:45 +0000

    Added tests

--- test/test.php
+++ test/test.php
@@ -1,10 +1,14 @@
 <?php
 
+$p = Panel::getById("PIGEN200");
 highlight_string("<?php\n\n\$x = ".var_export(array(
 	'isMobileEnabled' => Web::getSettings()->mobileEnabled,
 	'isMobile' => Web::getClient()->mobile,
 	'isWindowPhone' => Web::getClient()->windowsPhone,
 	'isAndroid' => Web::getClient()->android,
 	'isIPad' => Web::getClient()->iPad,
+	'PIGEN200' => $p,
+	'mobile address' => $p->getAddress(true, Web::DOMAIN_TYPE_MOBILE),
+	'mobile domain' => Web::getDomain(Web::DOMAIN_TYPE_MOBILE),
 ), true)."\n\n?>");
 
