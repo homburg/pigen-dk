4b17db5c7213ef05a104023b155d2133b074ae82
Thomas B Homburg <thomas@homburg.dk>
2012-03-11 01:13:05 +0100

    Fixed bugs

--- test/test.php
+++ test/test.php
@@ -1,13 +1,16 @@
 <?php
 
-$p = Panel::getById("PIGEN200");
+$list = Panel::getList();
+$i = rand(0, count($list)-1);
+$list = array_values($list);
+$p = Panel::getById($list[$i]);
 highlight_string("<?php\n\n\$x = ".var_export(array(
 	'isMobileEnabled' => Web::getSettings()->mobileEnabled,
 	'isMobile' => Web::getClient()->mobile,
 	'isWindowPhone' => Web::getClient()->windowsPhone,
 	'isAndroid' => Web::getClient()->android,
 	'isIPad' => Web::getClient()->iPad,
-	'PIGEN200' => $p,
+	'Panel' => $p,
 	'mobile address' => $p->getAddress(true, Web::DOMAIN_TYPE_MOBILE),
 	'mobile domain' => Web::getDomain(Web::DOMAIN_TYPE_MOBILE),
 ), true)."\n\n?>");
